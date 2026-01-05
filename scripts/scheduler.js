77f6db02001362b3a087b9dca777a2da928eeeac
bakomon <mybakomon@gmail.com>
2026-01-05 10:03:54 +0800

    Disable: Zampto

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -11,8 +11,7 @@ const config = [
   { workflow: "alwaysdata-renew.yml", interval: { value: 85, unit: "day" } },
   { workflow: "heliohost-renew.yml", interval: { value: 25, unit: "day" } },
   { workflow: "sprinthost-renew.yml", interval: { value: 55, unit: "day" } },
-  { workflow: "webhostmost-renew.yml", interval: { value: 40, unit: "day" } },
-  { workflow: "zampto-renew.yml", interval: { value: 26, unit: "hour" } }
+  { workflow: "webhostmost-renew.yml", interval: { value: 40, unit: "day" } }
   // netsons.com - every year, expiration: 03/08/2026
 ];
 
